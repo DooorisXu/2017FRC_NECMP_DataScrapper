c1969898041194da5d836e6d0ae358ef187b7488
DooorisXu <lingyi19329@gmail.com>
2018-01-16 16:23:34 -0500

    some edits

--- Scrapper2.py
+++ Scrapper2.py
@@ -4,4 +4,10 @@ import csv
 import urllib2
 
 api_token = "RhaEiTQBNGNWZxnEfJhDpU3HmLbFTpz6Sw0HxAX9GCqtCrz1KqPEyfpgKiMrAOIY"
-api_url = "https://www.thebluealliance.com/api/v3/"
+api_url_base = "https://www.thebluealliance.com/api/v3/"
+
+headers = {"contentType": "application/json",
+           "authorization": api_url
+           }
+def get_content ():
+    //api_url_base =
