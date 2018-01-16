91219446a5f6d96b91d70c1bf7185b5db24e1875
DooorisXu <lingyi19329@gmail.com>
2018-01-16 16:15:39 -0500

    started another scrapper using the blue alliance api

---
+++ Scrapper2.py
@@ -0,0 +1,7 @@
+import requests
+import json
+import csv
+import urllib2
+
+api_token = "RhaEiTQBNGNWZxnEfJhDpU3HmLbFTpz6Sw0HxAX9GCqtCrz1KqPEyfpgKiMrAOIY"
+api_url = "https://www.thebluealliance.com/api/v3/"
